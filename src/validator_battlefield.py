340fd7d1404c0e97cd438112e3709063ecc8a06a
skeriy <seergoo117@gmail.com>
2018-05-04 16:35:58 +0300

    correction_validator

--- src/validator_battlefield.py
+++ src/validator_battlefield.py
@@ -1,4 +1,4 @@
-#Поле для проверки на правильную расстановку кораблей
+# Поле для проверки на правильную расстановку кораблей
 grid = [[1,0,0,0,0,0,1,1,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,1,1,0,1,0,0,0,0,0],
@@ -6,14 +6,15 @@ grid = [[1,0,0,0,0,0,1,1,0,0],
         [0,0,0,0,1,0,0,0,1,0],
         [1,1,1,0,1,0,0,0,1,0],
         [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,1,1,1,1,0,0],
+        [0,0,0,0,0,0,1,1,1,1],
         [0,0,0,0,0,0,0,0,0,0],
         [0,1,0,0,0,0,0,0,0,1]]
 
-#количество кораблей
-number_of_ships = [0,0,0,0]
+# количество кораблей
+number_of_ships = [0, 0, 0, 0, 0]
 
-#Подсчет кораблей на поле
+
+# Подсчет кораблей на поле
 def num_of_ships(count, number):
     if count == 1:
         number[0] += 1
@@ -23,54 +24,60 @@ def num_of_ships(count, number):
         number[2] += 1
     elif count == 4:
         number[3] += 1
+    else:
+        number[4] = 1
 
 
-#для хранения проверенных ячеек
+# для хранения проверенных ячеек
 checked_position = []
 
-#для хранения списков содержащих ячейки корабля
+# для хранения списков содержащих ячейки корабля
 ships_coordinates = []
 
-#функция-валидатор поля
+
+# функция-валидатор поля
 def validate_grid(grid):
     i = 0
     j = 0
-    #проходим по всем ячейкам и заносим координаты кораблей
+    # проходим по всем ячейкам и заносим координаты кораблей
     while i < 10:
         while j < 10:
-            #если найдена палуба корабля и в списке проверенных ячеек, нет этой ячейки
+            # если найдена палуба корабля и в списке проверенных ячеек, нет этой ячейки
             if (grid[i][j] == 1) & (checked_position.count([i, j]) == 0):
                 temp = []
                 ti = i
                 tj = j
                 count = 0
-                #если не выходим за границу сетки и следующая вниз ячейка является продолжением корабля
+                # если не выходим за границу сетки и следующая вниз ячейка является продолжением корабля
                 if (i + 1 < 10) and (grid[i + 1][j] == 1):
                     while grid[ti][tj] != 0:
                         checked_position.append([ti, tj])
                         temp.append([ti, tj])
-                        ti += 1
-                        if ti >= 10:
+                        if ti >= 9:
+                            count += 1
                             break
+                        ti += 1
                         count += 1
                     j += 1
                     ships_coordinates.append(temp)
-                    num_of_ships(count,number_of_ships)
+                    num_of_ships(count, number_of_ships)
                 # если не выходим за границу сетки и следующая вправо ячейка является продолжением корабля
                 elif (j + 1 < 10) and (grid[i][j + 1] == 1):
                     while grid[ti][tj] != 0:
                         checked_position.append([ti, tj])
                         temp.append([ti, tj])
-                        tj += 1
-                        if tj >= 10:
+                        if tj >= 9:
+                            count += 1
                             break
+                        tj += 1
                         count += 1
                     j = tj
                     ships_coordinates.append(temp)
                     num_of_ships(count, number_of_ships)
-                #иначе корабль однопалубный
+                # иначе корабль однопалубный
                 else:
                     checked_position.append([ti, tj])
+                    ships_coordinates.append([[ti, tj]])
                     j = tj + 1
                     num_of_ships(1, number_of_ships)
             else:
@@ -80,31 +87,37 @@ def validate_grid(grid):
 
     if (number_of_ships[0] == 4) & (number_of_ships[1] == 3) & (number_of_ships[2] == 2) & (number_of_ships[3] == 1):
         print("Все корабли расставлены")
+    elif number_of_ships[4] == 1:
+        print("На поле находятся корабли больше 4-х палубного")
+        return False
     else:
         print("Не хватает или много расставленных кораблей! Кораблей на поле:\n"
-              "1-палубных: {},\n2-палубных: {},\n3-палубных: {},\n4-палубных: {}".format(number_of_ships[0],number_of_ships[1],number_of_ships[2],number_of_ships[3]))
-
-    #цикл для прохода по найденым кораблям
+              "1-палубных: {},\n2-палубных: {},\n3-палубных: {},\n4-палубных: {}".format(number_of_ships[0],
+                                                                                         number_of_ships[1],
+                                                                                         number_of_ships[2],
+                                                                                         number_of_ships[3]))
+        return False
+    # цикл для прохода по найденым кораблям
     for num in range(len(ships_coordinates)):
 
         whole_ship = ships_coordinates[num]
         ship_len = len(whole_ship)
 
-        #проходим по каждой палубе и смотрим нет ли вокруг нее других кораблей
+        # проходим по каждой палубе и смотрим нет ли вокруг нее других кораблей
         for first_arg in range(ship_len):
-            #координаты палубы
+            # координаты палубы
             x = whole_ship[first_arg][0]
             y = whole_ship[first_arg][1]
-            #циклы для обхода пространства вокруг палубы
+            # циклы для обхода пространства вокруг палубы
             for zx in range(-1, 2):
                 for zy in range(-1, 2):
-                    #вспомогательные переменные проверяемой ячейки
+                    # вспомогательные переменные проверяемой ячейки
                     nx = x + zx
                     ny = y + zy
-                    #если проверяемая ячейка находится на поле
+                    # если проверяемая ячейка находится на поле
                     if (nx >= 0) and (nx < 10):
                         if (ny >= 0) and (ny < 10):
-                            #если проверяемая ячейка является палкубой корабля
+                            # если проверяемая ячейка является палкубой корабля
                             if whole_ship.count([nx, ny]) == 1:
                                 pass
                             else:
@@ -116,6 +129,7 @@ def validate_grid(grid):
                                     return False
 
     return True
+
 
 isValidate = validate_grid(grid)
 print(isValidate)
